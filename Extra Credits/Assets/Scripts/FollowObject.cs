9c869f95d9e2f2517fb0d89e83f08c7224dd2731
Harry Bullen <hbullen@gmail.com>
2020-08-21 18:48:08 -0400

    Made player that moves and has a target to get to.

---
+++ Extra Credits/Assets/Scripts/FollowObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject: MonoBehaviour
+{
+
+	public GameObject target;
+
+	// Update is called once per frame
+	void Update() {
+		// Update 2d pos to follow target
+    // Horrible should use position transform.position
+		transform.SetPositionAndRotation(new Vector3( target.transform.localPosition.x,
+		                                              target.transform.localPosition.y,
+		                                              transform.localPosition.z),
+		                                 transform.rotation);
+	}
+}
---
+++ Extra Credits/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler: MonoBehaviour
+{
+
+	public float forwardV = 1.0f;
+	public float backwardV = 0.5f;
+	public float rotV = 1.0f;
+
+	private Rigidbody2D m_Rigidbody2D;
+	// Start is called before the first frame update
+	void Start() {
+		m_Rigidbody2D = GetComponent < Rigidbody2D > ();
+	}
+
+	private float velocityV = 0f;
+	private float angularV = 0f; // degrees per second
+
+	void Update() {
+
+		// Use Input's sensistive so that movement is not instattanious
+		// default to 0 to 100 in 1/3rd of a second.
+		float vAxis = Input.GetAxis("Vertical");
+
+		velocityV = 0f; // Hopefully no threading issues
+		if ( vAxis > 0 )
+			velocityV = vAxis * forwardV;
+		if ( vAxis < 0 )
+			velocityV = vAxis * backwardV;
+
+		angularV = Input.GetAxis("Horizontal") * -rotV;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate() {
+
+		// tranform.up gives green axis in world units which is then multiplied by
+		// 1 dimensional velocity to get the two dimensional velocity.
+		m_Rigidbody2D.velocity = transform.up * velocityV;
+		m_Rigidbody2D.angularVelocity = angularV;
+	}
+}
