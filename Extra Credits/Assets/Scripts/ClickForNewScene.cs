086cd14f0eb91c1ad8bdd883355a06125883d0e7
Harry Bullen <hbullen@gmail.com>
2020-08-21 13:56:06 -0400

    Sample Minimial Scenes
    A start scene with a button.

---
+++ Extra Credits/Assets/Scripts/ClickForNewScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickForNewScene: MonoBehaviour
+{
+	public void NextScene(string scene) {
+		// Switch to scene specified by string.
+		SceneManager.LoadScene(scene);
+	}
+
+}
