62d48d6932c05143264a30c6e87a9f2ced7f072c
Harry Bullen <hbullen@gmail.com>
2020-08-24 12:31:03 -0400

    Mostly Good minus sprites

---
+++ Extra Credits/Assets/Scripts/MonsterAi.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class MonsterAi: MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	public void OnPathComplete(Path p) {
+		Debug.Log("A path was calculated. Did it fail with an error? " + p.error);
+
+	}
+}
---
+++ Extra Credits/Assets/Scripts/PlayerRandomizer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRandomizer: MonoBehaviour
+{
+	public Transform [] startingPoints;
+	// Start is called before the first frame update
+	void Start() {
+		GameObject p = GameObject.FindWithTag("Player");
+		Transform t = startingPoints[RandomWholeNum(startingPoints.Length)];
+		p.transform.position = t.position;
+		p.transform.rotation = t.rotation;
+
+	}
+
+	int RandomWholeNum(int max) {
+		// Max is exclsive
+		return Mathf.FloorToInt(Random.value * max);
+	}
+
+}
